d3156ee706ab18e6f0ec8a0436c8b9ed576a5afd
Isabel <i.r.bavian@hotmail.com>
2020-05-03 00:45:11 +0200

    prject 2 added

---
+++ 2_time-is-running-out/index.js
@@ -0,0 +1,6 @@
+const explanationText = document.querySelector("#explanation-case");
+const explanationTextPart1 = document.querySelector("#explanation-case");
+const explanationTextPart2 = document.querySelector("#explanation-case");
+
+
+// explanationText.style.display = "none";
