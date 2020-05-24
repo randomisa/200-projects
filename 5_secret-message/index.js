ca418e5e41cbec27624099ed3dec0948817afcf7
Isabel <i.r.bavian@hotmail.com>
2020-05-24 21:13:53 +0200

    managed to get scroll effect. Need to fix some small things as scrolling distance etc.

--- 5_secret-message/index.js
+++ 5_secret-message/index.js
@@ -4,7 +4,7 @@ DOM
 textPartOne = document.querySelector(".text-part-one")
 textPartTwo = document.querySelector(".text-part-two")
 
-const textArray = [
+let songTextArray = [
     "We're no strangers to love",
     "You know the rules and so do I",
     "A full commitment's what I'm thinking of",
@@ -12,7 +12,37 @@ const textArray = [
     "Gotta make you understand"
 ]
 
-console.log(textArray)
+
+isEvenArray(textPartOne, songTextArray)
+isOddArray(textPartTwo, songTextArray)
+
+function isEvenArray(domElement, array) {
+
+    array.forEach(str => {
+        let newStr = str.split("").map((letter, i) => i % 2 ? "&nbsp" : letter).join("") + "<br>";
+
+        domElement.innerHTML += newStr;
+    });
+    // for (let i = 0; i < array.length; i++) {
+    //     return array[i].split("").map((letter, i) => i % 2 ? " " : letter).join("") + "<br>";
+    // }
+}
+
+
+function isOddArray(domElement, array) {
+
+    array.forEach(str => {
+        let newStr = str.split("").map((letter, i) => i % 2 ? letter : "&nbsp").join("") + "<br>";;
+
+        domElement.innerHTML += newStr;
+    });
+    // for (let i = 0; i < array.length; i++) {
+    //     return array[i].split("").map((letter, i) => i % 2 ? " " : letter).join("") + "<br>";
+    // }
+}
+
+
+
 
 const textString = 
 "We're no strangers to love_\
@@ -26,12 +56,12 @@ let strPartOne = textString;
 let strPartTwo = textString;
 
 
-textPartOne.innerHTML = isEven(strPartOne);
-textPartTwo.innerHTML = isOdd(strPartTwo);
+// textPartOne.innerHTML = isEven(strPartOne);
+// textPartTwo.innerHTML = isOdd(strPartTwo);
 
 
-lineBreak(textPartOne)
-lineBreak(textPartTwo)
+// lineBreak(textPartOne)
+// lineBreak(textPartTwo)
 
 function isEven(str) {
     return str.split("").map((letter, i) => i % 2 ? " " : letter).join("");
