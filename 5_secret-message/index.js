c26e901623ee246ebd1ad23ef8ab8a0e1e420836
Isabel <i.r.bavian@hotmail.com>
2020-05-26 23:14:51 +0200

    added second part of text and added YT Video. Fixed mobile scrolling with minimum-scale

--- 5_secret-message/index.js
+++ 5_secret-message/index.js
@@ -9,7 +9,17 @@ let songTextArray = [
     "You know the rules and so do I",
     "A full commitment's what I'm thinking of",
     "I just wanna tell you how I'm feeling",
-    "Gotta make you understand"
+    "Gotta make you understand",
+    "",
+    "I just wanna tell you how I'm feeling",
+    "Gotta make you understand",
+    "",
+    "Never gonna give you up",
+    "Never gonna let you down",
+    "Never gonna run around and desert you",
+    "Never gonna make you cry",
+    "Never gonna say goodbye",
+    "Never gonna tell a lie and hurt you"
 ]
 
 
